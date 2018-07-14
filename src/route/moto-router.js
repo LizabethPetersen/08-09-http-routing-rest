7d087d96497d6ea1fef9a6813c79f90eb5f2298a
Liz Petersen <lizpetersen928@gmail.com>
2018-07-14 10:37:51 -0700

    updates moto-router

--- src/route/moto-router.js
+++ src/route/moto-router.js
@@ -6,7 +6,7 @@ const customResponse = require('../lib/response');
 
 module.exports = (router) => {
   router.post('/api/v1/motorcycles', (request, response) => {
-    logger.log(logger.INFO, 'ROUTE-NOTE: POST /api/v1/motorcycles');
+    logger.log(logger.INFO, 'ROUTE: POST /api/v1/motorcycles');
     const newMoto = new Moto(request.body);
     newMoto.save()
       .then((moto) => {
@@ -14,7 +14,7 @@ module.exports = (router) => {
         return undefined;
       })
       .catch((err) => {
-        logger.log(logger.INFO, `ROUTE NOTE: There was a bad request ${JSON.stringify(err.message)}`);
+        logger.log(logger.INFO, `ROUTE: There was a bad request ${JSON.stringify(err.message)}`);
         customResponse.sendError(response, 400, err.message);
         return undefined;
       });
